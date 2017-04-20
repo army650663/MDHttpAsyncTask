738e82c48a59bdc2f3e22febf0c254de49aca554
MadmanChen <madmanchen@hsinten.com.tw>
2017-04-20 14:44:42 +0800

    Create

---
+++ sample/src/main/java/tw/idv/madmanchen/mdhttpasynctask/MainActivity.java
@@ -0,0 +1,13 @@
+package tw.idv.madmanchen.mdhttpasynctask;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
